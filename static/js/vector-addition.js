cc239b55ecfe05a2d4a18137bcfb69bac419b1e2
Luke Henderson <henderson.luket@gmail.com>
2022-09-26 05:02:45 -0700

    worked on js for vector addition

--- static/js/vector-addition.js
+++ static/js/vector-addition.js
@@ -1,33 +1,53 @@
 document.addEventListener ('alpine:init', () => {
-    Alpine.data('vecNumber', () => ({
-        value: 2,
+    Alpine.data('main', () => ({
 
-        numsub () {
-            if ( this.value <= 1 ){
+        values: [
+         [0, 0, 0],
+         [0, 0, 0]
+        ],
+
+        vecsub () {
+            if ( Object.keys(this.values).length <= 1 ) {
                 alert("Stop that.");
             } else {
-                this.value --;
+                this.values.pop();
+            }
+        },
+        vecadd () {
+            this.values.push([]);
+            for (let i = 0; i < this.values[0].length; i++) {
+                this.values[this.values.length - 1].push(0);
             }
         },
 
-        numadd () {
-            this.value ++;
-        },
-    }))
-    Alpine.data ('dimensions', () => ({
-        value: 3,
-        
-        numsub () {
-            if ( this.value <= 1 ) {
-                alert("You think you're funny?")
+        dimsub () {
+            if ( this.values[0].length <= 1 ) {
+                alert("You think you're funny?");
             } else {
-                this.value --;
+                for (let i = 0; i < this.values.length; i++) {
+                    this.values[i].pop();
+                }
+            }
+        },
+        dimadd () {
+            for (let i = 0; i < this.values.length; i++) {
+                this.values[i].push(0);
             }
         },
 
-        numadd () {
-            this.value ++;
+        result () {
+            result = [];
+            for(let i = 0; i < this.values[0].length; i++) {
+                for (let j = 0; j < this.values.length; j++) {
+                    x = 0;
+                    x += this.values[j][i];
+                    result.push(x);
+                    console.log(result);
+                }
+            }
+            return result;
         }
+
     }))
 })
 
