f6d5152f15502164f7a423e1b7e25e4a9ff8033c
Luke Henderson <henderson.luket@gmail.com>
2022-09-30 22:57:03 -0700

    idk

--- app.py
+++ app.py
@@ -1,117 +1,101 @@
 from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
+## HOME
 @app.route("/")
 def home():
 	return(render_template("index.html"))
 
+## MATH
 @app.route("/Math")
 def math():
     return(render_template("math.html"))
 
+## VECTOR CALCULUS
 @app.route("/Math/Vector-Calc")
 def vectorCalc():
     return(render_template("vector-calc.html"))
-@app.route("/Vector-Calc")
-def vectorCalcRedirect():
-    return(redirect("/Math/Vector-Calc"))
 
 @app.route("/Math/Vector-Calc/Vector-Addition")
 def vectorAddition():
     return(render_template("vector-addition.html"))
-@app.route("/Vector-Calc/Vector-Addition")
-@app.route("/Math/Vector-Addition")
-@app.route("/Vector-Addition")
-def vectorAdditionRedirect():
-    return(redirect("/Math/Vector-Calc/Vector-Addition"))
 
 @app.route("/Math/Vector-Calc/Scalar-Multiplication")
 def scalarMultiplication():
     return(render_template("scalar-multiplication.html"))
-@app.route("/Vector-Calc/Scalar-Multiplication")
-@app.route("/Math/Scalar-Multiplication")
-@app.route("/Scalar-Multiplication")
-def scalarMultiplicationRedirect():
-    return(redirect("/Math/Vector-Calc/Scalar-Multiplication"))
 
 @app.route("/Math/Vector-Calc/Vector-Magnitude")
 def vectorMagnitude():
     return(render_template("vector-magnitude.html"))
-@app.route("/Vector-Calc/Vector-Magnitude")
-@app.route("/Math/Vector-Magnitude")
-@app.route("/Vector-Magnitude")
-@app.route("/Vector-Calc/Magnitude")
-@app.route("/Math/Magnitude")
-@app.route("/Magnitude")
-def vectorMagnitudeRedirect():
-    return(redirect("/Math/Vector-Calc/Vector-Magnitude"))
 
 @app.route("/Math/Vector-Calc/Unit-Vector")
 def unitVector():
     return(render_template("unit-vector.html"))
-@app.route("/Vector-Calc/Unit-Vector")
-@app.route("/Math/Unit-Vector")
-@app.route("/Unit-Vector")
-def unitVectorRedirect():
-    return(redirect("/Math/Vector-Calc/Unit-Vector"))
 
 @app.route("/Math/Vector-Calc/Dot-Product")
 def dotProduct():
     return(render_template("dot-product.html"))
-@app.route("/Vector-Calc/Dot-Product")
-@app.route("/Math/Dot-Product")
-@app.route("/Dot-Product")
-def dotProductRedirect():
-    return(redirect("/Math/Vector-Calc/Dot-Product"))
 
 @app.route("/Math/Vector-Calc/Cross-Product")
 def crossProduct():
     return(render_template("cross-product.html"))
-@app.route("/Vector-Calc/Cross-Product")
-@app.route("/Math/Cross-Product")
-@app.route("/Cross-Product")
-def crossProductRedirect():
-    return(redirect("/Math/Vector-Calc/Cross-Product"))
 
 @app.route("/Math/Vector-Calc/TSP")
 def TSP():
     return(render_template("tsp.html"))
-@app.route("/Math/Vector-Calculus/Triple-Scalar-Product")
-@app.route("/Vector-Calc/Triple-Scalar-Product")
-@app.route("/Math/Triple-ScalarProduct")
-@app.route("/Triple-Scalar-Product")
-@app.route("/Vector-Calc/TSP")
-@app.route("/Math/TSP")
-@app.route("/TSP")
-def TSPRedirect():
-    return(redirect("/Math/Vector-Calc/TSP"))
 
 @app.route("/Math/Vector-Calc/Vector-Projection")
 def vectorProjection():
     return(render_template("under-construction.html"))
-@app.route("/Vector-Calc/Vector-Projection")
-@app.route("/Math/Vector-Projection")
-@app.route("/Vector-Projection")
-def vectorProjectionRedirect():
-    return(redirect("/Math/Vector-Calc/Vector-Projection"))
 
 @app.route("/Math/Vector-Calc/Line-Equation")
 def lineEquation():
     return(render_template("under-construction.html"))
-@app.route("/Vector-Calc/Line-Equation")
-@app.route("/Math/Line-Equation")
-@app.route("/Line-Equation")
-def lineEquationRedirect():
-    return(redirect("/Math/Vector-Calc/Line-Equation"))
 
 @app.route("/Math/Vector-Calc/Distance")
 def distance():
     return(render_template("under-construction.html"))
-@app.route("/Vector-Calc/Distance")
-@app.route("/Math/Distance")
-@app.route("/Distance")
-def distanceRedirect():
-    return(redirect("/Math/Vector-Calc/Distance"))
+
+## PROBABILITY AND STATISTICS
+@app.route("/Math/Prob-Stat")
+def probStat():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Inclusion-Exclusion")
+def incEx():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Independence-Dependence")
+def indepDep():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Conditional-Probability")
+def conditionalProb():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Bayes-Theorem")
+def bayesTheorem():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Mean-Median")
+def meanMedian():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Variance")
+def variance():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Expectation")
+def expectation():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Binomial-Distribution")
+def binomialDistribution():
+    return(render_template("under-construction.html"))
+
+@app.route("/Math/Prob-Stat/Cumulative-Probabilities")
+def cumProb():
+    return(render_template("under-construction.html"))
 
 if(__name__ == "__main__"):
 	app.run(host="0.0.0.0")
