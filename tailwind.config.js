4e5336b33519fd699db3e3ac6fae17350e6a9a04
Luke Henderson <personal@lukeh.us>
2025-09-23 03:38:24 -0700

    math tab

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: [
-        "./website/templates/**/*.{html,jinja,jinja2",
-        "./website/static/js/**/*.{js,ts}"
-    ],
-    theme : {extend: {}},
-    plugins: []
-}
