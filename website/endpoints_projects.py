35537991d7dcd42c7fe4cb1961b20540c63659d1
Luke Henderson <personal@lukeh.us>
2025-12-15 09:24:07 -0800

    feat/contact: rename asmadi

--- website/endpoints_projects.py
+++ website/endpoints_projects.py
@@ -1,31 +1,68 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 endpoints_projects = Blueprint("endpoints_projects", __name__)
 
 @endpoints_projects.route("/")
 def projects():
-    return("incomplete")
+    """
+    Projects directory page
+    """
+
+    return "incomplete"
 
 @endpoints_projects.route("/Lukeh.xyz")
 def portfolio():
-    return(render_template("projects/portfolio.html"))
+    """
+    Portfolio
+    """
+
+    return render_template("projects/portfolio.html")
 
 @endpoints_projects.route("/SD-Runpod")
 def sd_runpod():
-    return(render_template("projects/sd_runpod.html"))
+    """
+    SD Runpod
+    """
+
+    return render_template("projects/sd_runpod.html")
 
 @endpoints_projects.route("/Silverquill-Compiler")
 def silverquill():
-    return(render_template("projects/silverquill.html"))
+    """
+    Silverquill
+    """
+
+    return render_template("projects/silverquill.html")
 
 @endpoints_projects.route("/Asmadi-Engine")
 def asmadi():
-    return(render_template("projects/asmadi.html"))
+    """
+    Asmadi was renamed
+    """
+
+    return redirect(url_for("endpoints_projects.palladia"))
+
+@endpoints_projects.route("/Palladia-Engine")
+def palladia():
+    """
+    Palladia engine
+    (formerly Asmadi)
+    """
+
+    return render_template("projects/palladia.html")
 
 @endpoints_projects.route("/Math")
 def math():
-    return(render_template("projects/math.html"))
+    """
+    Math tools
+    """
+
+    return render_template("projects/math.html")
 
 @endpoints_projects.route("/Other")
 def other():
-    return(render_template("projects/other.html"))
+    """
+    Other projects
+    """
+
+    return render_template("projects/other.html")
