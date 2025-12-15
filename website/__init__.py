720e3f109643e48f901d68143a116ce0cedced82
Luke Henderson <personal@lukeh.us>
2025-12-15 10:12:01 -0800

    feat/contact: backend functionality for contact form

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,48 @@
+import os
+import smtplib
+
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
+SMTP_SERVER = os.getenv("SMTP_SERVER", None)
+if SMTP_SERVER is None:
+    raise ValueError("SMTP_SERVER environment variable cannot be None.")
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_USERNAME = os.getenv("SMTP_USERNAME", None)
+if SMTP_USERNAME is None:
+    raise ValueError("SMTP_USERNAME environment variable cannot be None.")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", None)
+if SMTP_PASSWORD is None:
+    raise ValueError("SMTP_PASSWORD environment variable cannot be None.")
+SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "true").casefold() == "true"
+FROM_EMAIL = os.getenv("FROM_EMAIL", None)
+if FROM_EMAIL is None:
+    raise ValueError("FROM_EMAIL environment variable cannot be None.")
+FROM_NAME = os.getenv("FROM_NAME", None)
+if FROM_NAME is None:
+    raise ValueError("FROM_NAME environment variable cannot be None.")
+TO_EMAIL = os.getenv("TO_EMAIL", None)
+if TO_EMAIL is None:
+    raise ValueError("TO_EMAIL environment variable cannot be None.")
+
 def start():
+    """
+    Create Flask app object
+    """
+
     app = Flask(__name__)
+
+    smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    if SMTP_USE_TLS:
+        smtp.starttls()
+    smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
+
+    app.config["smtp"] = smtp
+    app.config["from_email"] = FROM_EMAIL
+    app.config["from_name"] = FROM_NAME
+    app.config["to_email"] = TO_EMAIL
 
     from .endpoints_main import endpoints_main
     # from .endpoints_auth import endpoints_auth
