6bbe56ec429936c0686b22eebcdcac644afa98b9
Luke Henderson <personal@lukeh.us>
2025-12-17 07:43:07 -0800

    bug/502: reintroduce environment variables

--- website/__init__.py
+++ website/__init__.py
@@ -1,31 +1,31 @@
 import os
 # import smtplib
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from flask import Flask
 
-# load_dotenv()
+load_dotenv()
 
-# SMTP_SERVER = os.getenv("SMTP_SERVER", None)
-# if SMTP_SERVER is None:
-#     raise ValueError("SMTP_SERVER environment variable cannot be None.")
-# SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
-# SMTP_USERNAME = os.getenv("SMTP_USERNAME", None)
-# if SMTP_USERNAME is None:
-#     raise ValueError("SMTP_USERNAME environment variable cannot be None.")
-# SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", None)
-# if SMTP_PASSWORD is None:
-#     raise ValueError("SMTP_PASSWORD environment variable cannot be None.")
-# SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "true").casefold() == "true"
-# FROM_EMAIL = os.getenv("FROM_EMAIL", None)
-# if FROM_EMAIL is None:
-#     raise ValueError("FROM_EMAIL environment variable cannot be None.")
-# FROM_NAME = os.getenv("FROM_NAME", None)
-# if FROM_NAME is None:
-#     raise ValueError("FROM_NAME environment variable cannot be None.")
-# TO_EMAIL = os.getenv("TO_EMAIL", None)
-# if TO_EMAIL is None:
-#     raise ValueError("TO_EMAIL environment variable cannot be None.")
+SMTP_SERVER = os.getenv("SMTP_SERVER", None)
+if SMTP_SERVER is None:
+    raise ValueError("SMTP_SERVER environment variable cannot be None.")
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_USERNAME = os.getenv("SMTP_USERNAME", None)
+if SMTP_USERNAME is None:
+    raise ValueError("SMTP_USERNAME environment variable cannot be None.")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", None)
+if SMTP_PASSWORD is None:
+    raise ValueError("SMTP_PASSWORD environment variable cannot be None.")
+SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "true").casefold() == "true"
+FROM_EMAIL = os.getenv("FROM_EMAIL", None)
+if FROM_EMAIL is None:
+    raise ValueError("FROM_EMAIL environment variable cannot be None.")
+FROM_NAME = os.getenv("FROM_NAME", None)
+if FROM_NAME is None:
+    raise ValueError("FROM_NAME environment variable cannot be None.")
+TO_EMAIL = os.getenv("TO_EMAIL", None)
+if TO_EMAIL is None:
+    raise ValueError("TO_EMAIL environment variable cannot be None.")
 
 FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "")
 
